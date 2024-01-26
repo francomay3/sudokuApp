02a02519299adc11d3ed42e8d99458d778955322
franco may <franco.may@etraveligroup.com>
2024-01-26 14:41:48 +0100

    created build configuration to export library

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,24 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/SudokuApp.tsx",
+      name: "SudokuApp",
+      fileName: (format) => `SudokuApp.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react-dom"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
